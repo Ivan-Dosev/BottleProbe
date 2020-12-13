7ae19d229670909776c618a2188372e6b16358d4
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-13 09:52:54 +0200

    Initial Commit

---
+++ BottleProbe/BottleProbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleProbeApp.swift
+//  BottleProbe
+//
+//  Created by Ivan Dimitrov on 13.12.20.
+//
+
+import SwiftUI
+
+@main
+struct BottleProbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
